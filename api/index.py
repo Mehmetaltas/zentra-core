1d2d14c0865173fa4d1be601ab285b5dca1ddce5
mehmet altaş <93026440+Mehmetaltas@users.noreply.github.com>
2026-03-27 11:50:42 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,10 +8,11 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://mehmetaltas.github.io",
+        "https://mehmetaltas.github.io/zentra-v2",
         "https://zentrarisk.com",
         "https://www.zentrarisk.com",
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
